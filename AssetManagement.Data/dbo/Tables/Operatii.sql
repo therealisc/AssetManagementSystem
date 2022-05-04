95ce3a8593a3958969653eded9c5f462dbb56d93
Ioan Scafa <ioan.scafa@soter.ro>
2022-05-04 11:54:42 +0300

    Fixed constraint

--- AssetManagement.Data/dbo/Tables/Operatii.sql
+++ AssetManagement.Data/dbo/Tables/Operatii.sql
@@ -4,5 +4,7 @@
     [ValoareOperatie] DECIMAL(18, 2) NOT NULL, 
     [DataEfectuariiOperatiei] DATETIME2 NOT NULL, 
     [IdTipOperatie] INT NOT NULL,
-    CONSTRAINT [FK_dbo.Operatii.TipuriOperatii_IdTipOperatie] FOREIGN KEY ([IdTipOperatie]) REFERENCES [dbo].[TipuriOperatii] ([IdTipOperatie]) ON DELETE CASCADE
+    [NumarInventar] INT NOT NULL, 
+    CONSTRAINT [FK_dbo.Operatii.TipuriOperatii_IdTipOperatie] FOREIGN KEY ([IdTipOperatie]) REFERENCES [dbo].[TipuriOperatii] ([IdTipOperatie]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.Operatii.MijloaceFixe_NumarInventar] FOREIGN KEY ([NumarInventar]) REFERENCES [dbo].[MijloaceFixe] ([NumarInventar]) ON DELETE CASCADE,
 )
