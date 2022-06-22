c08ccad5f3c3e9447f9c4d2e8821c30bd060bd2d
Ioan Scafa <ioan.scafa@soter.ro>
2022-06-22 19:46:34 +0300

    Add part of depreciation calculations

--- AssetManagement.Data/dbo/Stored Procedures/spDepreciationCalculation.sql
+++ AssetManagement.Data/dbo/Stored Procedures/spDepreciationCalculation.sql
@@ -3,8 +3,61 @@
 	@SelectedDate datetime2
 AS
 BEGIN
-	SET NOCOUNT ON;
+--------------------------------
 
-	SELECT * 
-	FROM MijloaceFixe
+	DECLARE @TabelIntrari TABLE (NumarInventar INT, DataIntrarii DATETIME2)
+
+	INSERT INTO @TabelIntrari (NumarInventar, DataIntrarii)
+		SELECT
+			MF.NumarInventar,
+
+			(SELECT MAX(DataDocument)
+			 FROM MijloaceFixe MFD
+			 INNER JOIN DocumenteMijloaceFixe DMF ON MFD.NumarInventar = DMF.NumarInventar
+			 INNER JOIN Documente DOC ON DOC.IdDocument = DMF.IdDocument
+			 INNER JOIN TipuriDocumente TD ON TD.IdTipDocument = DOC.IdTipDocument
+			 WHERE TD.TipOperatieDocument IN ('Intrare','Receptie') AND MFD.NumarInventar = MF.NumarInventar
+			 GROUP BY MFD.NumarInventar)
+		FROM
+			MijloaceFixe MF
+			INNER JOIN DocumenteMijloaceFixe DMF ON MF.NumarInventar = DMF.NumarInventar
+			INNER JOIN Documente DOC ON DMF.IdDocument = DOC.IdDocument
+		GROUP BY
+			MF.NumarInventar
+
+	------------------------------
+	
+	DECLARE @TabelIesiri TABLE (NumarInventar INT, DataIesirii DATETIME2)
+
+	INSERT INTO @TabelIesiri (NumarInventar, DataIesirii)
+		SELECT
+			MF.NumarInventar,
+
+			(SELECT MAX(DataDocument)
+			 FROM MijloaceFixe MFD
+			 INNER JOIN DocumenteMijloaceFixe DMF ON MFD.NumarInventar = DMF.NumarInventar
+			 INNER JOIN Documente DOC ON DOC.IdDocument = DMF.IdDocument
+			 INNER JOIN TipuriDocumente TD ON TD.IdTipDocument = DOC.IdTipDocument
+			 WHERE TD.TipOperatieDocument NOT IN ('Intrare','Receptie') AND MFD.NumarInventar = MF.NumarInventar
+			 GROUP BY MFD.NumarInventar)
+		FROM
+			MijloaceFixe MF
+			INNER JOIN DocumenteMijloaceFixe DMF ON MF.NumarInventar = DMF.NumarInventar
+			INNER JOIN Documente DOC ON DMF.IdDocument = DOC.IdDocument
+		GROUP BY
+			MF.NumarInventar
+
+
+
+
+	SELECT I.NumarInventar, I.DataIntrarii, 
+
+	--(SELECT DATEDIFF(M, I.DataIntrarii, IE.DataIesirii)) [LuniGestiune],
+	
+	IE.DataIesirii, MijloaceFixe.*
+	FROM @TabelIntrari I
+	INNER JOIN @TabelIesiri IE
+		ON I.NumarInventar = IE.NumarInventar
+		INNER JOIN MijloaceFixe ON MijloaceFixe.NumarInventar = I.NumarInventar
+
 END
